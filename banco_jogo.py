03b15ac368deac86598348883bf28f44196ff971
Rebeca Macedo Soares <rebecamssilva@gmail.com>
2023-11-17 11:36:53 -0300

    Changing the database.

--- banco_jogo.py
+++ banco_jogo.py
@@ -9,9 +9,9 @@ conexao = mysql.connector.connect(
 
 cursor = conexao.cursor()
 
-# cursor.execute("create database Pong")
+cursor.execute("create database Pong")
 
-# cursor.execute('CREATE TABLE jogo (id_jogo int(3) AUTO_INCREMENT PRIMARY KEY, tempo float(6), vencedor varchar(2))')
+cursor.execute('CREATE TABLE jogo (id_jogo int(3) AUTO_INCREMENT PRIMARY KEY, tempo float(6), vencedor varchar(2))')
 
 
 cursor.execute('SELECT * FROM jogo')
